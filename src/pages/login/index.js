31ebcbdbfc87a86d7cc14b2bebf6373aa267f513
macielv7 <macielvinicus500@gmail.com>
2022-09-13 16:31:50 -0300

    ok

---
+++ src/pages/login/index.js
@@ -0,0 +1,33 @@
+import './index.scss'
+import Cabecalho from '../../components/cabecalho'
+
+
+export default function Index() {
+    return (
+        <main className='page-login'>
+            <Cabecalho />
+
+            <div class="coonteudoo">
+                <div class="conteudo-formularioo">
+
+                    <div class="formularioo">
+                        <img src="./images/online_registration_form_and_Sign_in_button_generated" alt="" class="img-dental" />
+                        <h1 class="titulo">faça seu login</h1>
+                        <label>
+                            <input type="text" placeholder="Usuario" class="campo" />
+
+                            <input type="password" placeholder="Senha" class="campo" />
+
+                        </label>
+                        <button class="bntt"> LOGIN</button>
+                        <p class="campo-checkbox">Cadastre-se agora </p>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+        </main>
+    )
+}
