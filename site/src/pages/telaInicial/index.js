efccc9d4daf8d32cdc81064031974e0c0cbd31f0
Macielv7 <macielvinicius500@gmail.com>
2022-09-19 18:08:43 -0300

    ok

---
+++ site/src/pages/telaInicial/index.js
@@ -0,0 +1,136 @@
+
+import Menu from '../../components/menu'
+import './index.scss'
+
+
+
+export default function Index() {
+    return (
+        <nav className="comp-carrinho">
+             <div class="img">
+     <img src="https://s3-alpha-sig.figma.com/img/75c3/e9a7/4ba99cc15bdb8b9b9e75a2f6a1664ecf?Expires=1664150400&Signature=Dj45eIMpvqH1ogo-uFHurKoNCOdLx1jZLRhLvAUX4MfHbsCj5MFneyyWnV~MDDF4h2ZP2dNemnSwtSLueA6Ut~PapMQItKYFrVu96KyqYVFGMs6tavitFoBt0mLm-SwNLt3tZeAG0weeCD4n0mt0VZCcIumMPr7pwJ6m8eV8JJztWsRw0cohI7lm0i-4IQ8o~UOJmV-DXxpjCfXnJDbiOITvNWB6LOmyN8aBj~fokSscNnmFAvxb7tvQ9ELUr-8dikMn3pZ8hlnloQAQ0eVuIvv864nmiDSCRJF7A~QAfzrFuOs2PB2opF0z-eOyw22W8MSZL-eLIrKXobIhQ~DplQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" height="100%"  alt="" />
+   </div>
+
+   <div class="tt">
+    <h1>Departamento</h1>
+</div>
+
+    
+<div class="os">
+    
+    <div class="lk">
+        <h1>Gamer </h1>
+    <img src="./img/g.png" alt="" />
+   
+   
+    </div>
+
+    <div class="lk">
+        <h1>Telefones</h1>
+    <img  src="./img/g.png" alt="" />
+    
+   
+    </div>
+
+    <div class="lk" >
+        <h1>Casa inteligente</h1>
+    <img src="./img/g.png" alt="" />
+    
+    
+    </div>
+</div>
+
+
+
+<div class="os">
+    <div class="lk">
+        <h1>Gamer </h1>
+    <img src="./img/g.png" alt="" />
+   
+   
+    </div>
+
+    <div class="lk">
+        <h1>Telefones</h1>
+    <img  src="./img/g.png" alt="" />
+    
+   
+    </div>
+
+    <div class="lk" >
+        <h1>Casa inteligente</h1>
+    <img src="./img/g.png" alt="" />
+    
+    
+    </div>
+</div>
+
+<div class="os">
+    <div class="lk">
+        <h1>Gamer </h1>
+    <img src="./img/g.png" alt="" />
+   
+   
+    </div>
+
+    <div class="lk">
+        <h1>Telefones</h1>
+    <img  src="./img/g.png" alt="" />
+    
+   
+    </div>
+
+    <div class="lk" >
+        <h1>Casa inteligente</h1>
+    <img src="./img/g.png" alt="" />
+    
+    
+    </div>
+</div>
+
+<hr/>
+
+<div class="car">
+    <div >
+        <img src="/tudo/5/img/carr.png" alt=""/>
+    </div>
+    <div >
+       
+        <h3 >Frete Grátis para todo Brasil
+        </h3>
+    </div>
+
+    <div >
+        <img src="/tudo/5/img/carr.png" alt=""/>
+    </div>
+    <div >
+       
+        <h3 >5% de desconto no pix
+        </h3>
+    </div>
+
+    <div >
+        <img src="/tudo/5/img/carr.png" alt=""/>
+    </div>
+    <div >
+       
+        <h3 >Parcele em até 12x
+
+        </h3>
+    </div>
+
+    <div >
+        <img src="/tudo/5/img/carr.png" alt=""/>
+    </div>
+    <div >
+       
+        <h3 >Site seguro
+
+        </h3>
+    </div>
+    
+</div>
+<hr/>
+        </nav>
+    )
+}
