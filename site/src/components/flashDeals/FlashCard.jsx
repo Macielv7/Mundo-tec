491869bad9e10759fed33af26934a93562212d91
Macielv7 <macielvinicius500@gmail.com>
2022-09-28 14:35:09 -0300

    ok

--- site/src/components/flashDeals/FlashCard.jsx
+++ site/src/components/flashDeals/FlashCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SampleNextArrow = (props) => {
   const { onClick } = props
