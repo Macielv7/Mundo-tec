9479b129aab7862a0a4cc1487a189b2b70ec3137
macielv7 <macielvinicius500@gmail.com>
2022-10-28 11:20:51 -0300

    ok

--- site/src/pages/conta/Conta.js
+++ site/src/pages/conta/Conta.js
@@ -5,7 +5,7 @@ import "./index.scss"
 import { useEffect, useState } from 'react';
 
 import { listarProdutosInicio } from '../../api/produtoAPI'
-import Rodape from "../../components/rodape"
+
 import Header from "../../components/header"
 import FlashCard from "../../components/flashDeals/FlashCard"
 
@@ -199,7 +199,7 @@ export default function Index () {
                     </div>
                     <hr />
 
-               <Rodape/>
+              
 
                 </div>
             </div>
