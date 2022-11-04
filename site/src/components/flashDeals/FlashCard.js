220f83902a0fa381c0f270f3c7fd0fa1e853403f
Macielv7 <macielvinicius500@gmail.com>
2022-11-04 18:11:03 -0300

    ok

--- site/src/components/flashDeals/FlashCard.js
+++ site/src/components/flashDeals/FlashCard.js
@@ -45,7 +45,7 @@ export default function FlashCard(props) {
         )
     }
 
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState('🤍')
     const increment = () => {
         setCount(count + 1)
     }
