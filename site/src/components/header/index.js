38d951028824ad8b0abb0797fca5fc8a86ec0167
macielv7 <macielvinicius500@gmail.com>
2022-11-09 23:32:43 -0300

    ok

--- site/src/components/header/index.js
+++ site/src/components/header/index.js
@@ -16,15 +16,15 @@ export default function Header() {
             </label>
 
             <ul class="menu__box">
-              <li><a class="menu__item" href="/LoginUsuario">Minha Conta</a></li>
-              <li><a class="menu__item" href="/CadastroUsuario">Meu carrinho</a></li>
-              <li><a class="menu__item" href="/LoginAdm">Favoritos</a></li>
+              <li><a class="menu__item" href="/usuario1">Minha Conta</a></li>
+              <li><a class="menu__item" href="#">Meu carrinho</a></li>
+              <li><a class="menu__item" href="#">Favoritos</a></li>
               <hr />
-              <li><a class="menu__item" href="/LoginUsuario">Atendimento</a></li>
+              <li><a class="menu__item" href="#">Atendimento</a></li>
               <hr />
-              <li><a class="menu__item" href="/CadastroUsuario">Destaques</a></li>
-              <li><a class="menu__item" href="/LoginAdm">Lançamentos</a></li>
-              <li><a class="menu__item" href="/LoginAdm">Oferta do dia</a></li>
+              <li><a class="menu__item" href="#">Destaques</a></li>
+              <li><a class="menu__item" href="#">Lançamentos</a></li>
+              <li><a class="menu__item" href="#">Oferta do dia</a></li>
             </ul>
 
           </div>
--- site/src/pages/usuario/usuario1/index.js
+++ site/src/pages/usuario/usuario1/index.js
@@ -1,12 +1,71 @@
 import './index.scss'
+import Header from "../../../components/header"
+import Menu from "../../../components/menu"
 
 export default function Index() {
 
+    
+
     return (
-        <div >
+        <div className='usu1'>
+            <Header />
             
-          
+            <div className='mae'>
+
+                <div className='retangulo'>
+                    <div className='circo'></div>
+                    <h3>Bem-vindo, <br/><span>macielvinicius281@gmail.com</span></h3>
+                    
+                    <a href='#'>
+                    <img src="./img/icons8-segurança-verificada-24.png" className='' />
+                    </a>
+                </div>
+               
+                <div >
+                    <h6 className='fonte'>RESUMO DO SEU ÚLTIMO PEDIDO</h6>
+                </div>
+
+                <div className='geral-mom'>
+
+                    <div className='numero'>
+                        <p className='title'> NÚMERO DO PEDIDO </p>
+                        <p className='desc'> #30432868 </p>
+                    </div>
+
+
+                    <div className='status'>
+                        <p className='title'>STATUS</p>
+                        <p className='desc'>Cancelado </p>
+                    </div>
+
+                    <div className='data'>
+                        <p className='title'>DATA</p>
+                        <p className='desc'>09/11/2022</p>
+                    </div>
+
+                    <div className='pagamento'>
+                        <p className='title'>PAGAMENTO</p>
+                        <p className='desc'>PIX</p>
+                    </div>
+
+
+                </div>
+               <hr/>
+                <div className='geral-momm'>
+
+                    <div className='ender'>
+                        <p className='title'> ENDERECO </p>
+
+                        <div className='end'>numero 34 - jardin sipramar</div>
+                        <div className='cep'>cep 75674-756 sao paulo-sp</div>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
         </div>
     )
 }
-
